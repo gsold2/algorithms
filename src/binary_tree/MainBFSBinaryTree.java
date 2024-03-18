d29a9632dd93a1a336fa2acd00dca1ff31c11289
grigorii <gsold2@gmail.com>
2024-03-18 20:02:01 +0300

    add breadth for search binary tree

---
+++ src/binary_tree/MainBFSBinaryTree.java
@@ -0,0 +1,26 @@
+package binary_tree;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class MainBFSBinaryTree {
+    public static void main(String[] args) {
+        Node<Integer> root = BinaryTree.getRoot();
+        bfs(root);
+    }
+
+    private static void bfs(Node<Integer> start) {
+        Queue<Node<Integer>> queue = new ArrayDeque<>();
+        queue.add(start);
+
+        Node<Integer> currentNode;
+
+        while (!queue.isEmpty()) {
+            currentNode = queue.remove();
+
+            System.out.print(currentNode.getValue() + " ");
+            if (currentNode.getLeft() != null) queue.add(currentNode.getLeft());
+            if (currentNode.getRight() != null) queue.add(currentNode.getRight());
+        }
+    }
+}
